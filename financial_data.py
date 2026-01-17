60a0efaef753c739dc3beb55c66aa7e9d7398782
Sihle Ntuli <sihle.ntuli@transnet.net>
2026-01-17 18:50:32 +0200

    Added a function to collect divident ids and append them into an array

--- financial_data.py
+++ financial_data.py
@@ -7,6 +7,8 @@ load_dotenv(dotenv_path="./.env")
 
 Api_key = os.getenv("Api_key")
 
+max_result = 10
+
 def get_finance_id():
 
     try:
@@ -14,13 +16,28 @@ def get_finance_id():
         res = requests.get(url)
         
         data = res.json()
-
-        #print(json.dumps(data,indent=4))
-        print(data['results'][0]['id'])
+        print(json.dumps(data,indent=4))
+        #print(data['results'][0]['id'])   
         return data['results'][0]['id']
 
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+def get_divident_ids():
+    divident_ids = []
+    url = f'https://api.massive.com/v3/reference/dividends?apiKey={Api_key}'
+
+    try:
+        res = requests.get(url)
+        data = res.json()
+
+        for i in range(len(data['results'])):
+            divident_ids.append(data['results'][i]['id'])
+        return divident_ids
+    
     except requests.exceptions.RequestException as e:
         raise e
 
 if __name__ == "__main__":
     get_finance_id()
+    get_divident_ids()
